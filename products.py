b5fa544c5e689aba3d12f1f87e4ff246803829f7
Joe Chen <wentsaochen@gmail.com>
2021-11-03 11:56:27 +0800

    add check file function

--- products.py
+++ products.py
@@ -4,29 +4,32 @@ Created on Mon Nov  1 21:09:33 2021
 
 @author: Win10
 """
+import os # operating system
 # 讀取檔案
 products = []
-
-with open("products.csv", "r", encoding="utf-8") as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue 
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
-
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+    print('找到檔案了')
+    with open("products.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue 
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案')
+    
 # 讓使用者輸入
 while True:
-    name = input('請輸入商品名稱 :')
+    name = input('請輸入商品名稱 :  ')
     if name == 'q':
         break
-    price = input('請輸入商品價格 :')
+    price = input('請輸入商品價格 :  ')
    # p = []
    # p.append(name)
    # p.append(price)
     products.append([name, price])
 print(products)
-
 
 # 印出所有購買紀錄
 for product in products:
